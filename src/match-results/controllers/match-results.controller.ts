8ba8d24bb021a7c44a604caa573afe428a294349
Cristian Ortega <crisortegamunoz@gmail.com>
2025-07-20 18:10:42 -0400

    Termino de documentación en los controladores

--- src/match-results/controllers/match-results.controller.ts
+++ src/match-results/controllers/match-results.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { MatchResultsService } from '../service/match-results.service';
 import { CreateMatchResultDTO, UpdateMatchResultDTO } from '../dtos/matchResult.entity';
 import { Filter } from '../../common/filters/filter.dto';
@@ -7,32 +7,70 @@ import { Filter } from '../../common/filters/filter.dto';
 @ApiTags('match-results')
 @Controller('match-results')
 export class MatchResultsController {
-    
+
     constructor(private readonly matchesService: MatchResultsService) {
 
     }
 
     @Get()
+    @ApiOperation({ summary: 'Find match-results', description: 'Find a match-results from the database.' })
+    @ApiResponse({ status: 200, description: 'The match-results has been successfully found.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
     findAll(@Query() params: Filter) {
         return this.matchesService.findAll(params);
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Find a match-result', description: 'Find a match-result by id from the database.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match-result'
+    })
+    @ApiResponse({ status: 200, description: 'The match-result has been successfully found.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match-result not found' })
     findById(@Param('id') id: string) {
         return this.matchesService.findById(id);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create a new match-result', description: 'Adds a new match-result to the database.' })
+    @ApiBody({ type: CreateMatchResultDTO, description: 'Details of the match-result to be created.' })
+    @ApiResponse({ status: 201, description: 'The match-result has been successfully created.', type: CreateMatchResultDTO })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
     create(@Body() payload: CreateMatchResultDTO) {
         return this.matchesService.create(payload);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Update a match-result', description: 'Update a match-result by id from the database.' })
+    @ApiBody({ type: UpdateMatchResultDTO, description: 'Details of the match-result to be updated.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match-result'
+    })
+    @ApiResponse({ status: 200, description: 'The match-result has been successfully updated.', type: UpdateMatchResultDTO })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match-result not found' })
     update(@Param('id') id: string, @Body() payload: UpdateMatchResultDTO) {
         return this.matchesService.update(id, payload);
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete a match-result', description: 'Delete a match-result by id from the database.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match-result'
+    })
+    @ApiResponse({ status: 204, description: 'The match-result has been successfully deleted.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match-result not found' })
     delete(@Param('id') id: string) {
         return this.matchesService.delete(id);
     }
--- src/matches/controllers/matches.controller.ts
+++ src/matches/controllers/matches.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { Filter } from '../../common/filters/filter.dto';
 import { MatchesService } from '../service/matches.service';
@@ -14,26 +14,64 @@ export class MatchesController {
     }
 
     @Get()
+    @ApiOperation({ summary: 'Find matches', description: 'Find a matches from the database.' })
+    @ApiResponse({ status: 200, description: 'The matches has been successfully found.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
     findAll(@Query() params: Filter) {
         return this.matchesService.findAll(params);
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Find a match', description: 'Find a match by id from the database.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match'
+    })
+    @ApiResponse({ status: 200, description: 'The match has been successfully found.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match not found' })
     findById(@Param('id') id: string) {
         return this.matchesService.findById(id);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create a new match', description: 'Adds a new match to the database.' })
+    @ApiBody({ type: CreateMatchDTO, description: 'Details of the match to be created.' })
+    @ApiResponse({ status: 201, description: 'The match has been successfully created.', type: CreateMatchDTO })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
     create(@Body() payload: CreateMatchDTO) {
         return this.matchesService.create(payload);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Update a match', description: 'Update a match by id from the database.' })
+    @ApiBody({ type: UpdateMatchDTO, description: 'Details of the match to be updated.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match'
+    })
+    @ApiResponse({ status: 200, description: 'The match has been successfully updated.', type: UpdateMatchDTO })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match not found' })
     update(@Param('id') id: string, @Body() payload: UpdateMatchDTO) {
         return this.matchesService.update(id, payload);
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete a match', description: 'Delete a match by id from the database.' })
+    @ApiParam({
+        name: 'id',
+        required: true,
+        type: String,
+        description: 'Unique id from match'
+    })
+    @ApiResponse({ status: 204, description: 'The match has been successfully deleted.' })
+    @ApiResponse({ status: 400, description: 'Invalid input.' })
+    @ApiResponse({ status: 404, description: 'match not found' })
     delete(@Param('id') id: string) {
         return this.matchesService.delete(id);
     }
